e9b1f49a02f9ce2dc60e65f36879098a09c549fc
pyc <rtrtoioil@gmail.com>
2025-01-08 20:06:01 +0900

    1/8학원커밋

--- src/main/java/com/ict/edu/domain/auth/service/UserDetailService.java
+++ src/main/java/com/ict/edu/domain/auth/service/UserDetailService.java
@@ -1,6 +1,5 @@
 package com.ict.edu.domain.auth.service;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/ict/edu/domain/notice/controller/NoticeController.java
+++ src/main/java/com/ict/edu/domain/notice/controller/NoticeController.java
@@ -1,8 +1,31 @@
 package com.ict.edu.domain.notice.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ict.edu.domain.auth.vo.DataVO;
+import com.ict.edu.domain.notice.service.NoticeService;
+import com.ict.edu.domain.notice.vo.NoticeVO;
 
 @RestController
 public class NoticeController {
+    @Autowired
+    private NoticeService noticeService;
     
+    // 공지 리스트
+    public DataVO getNoticeList(){
+        DataVO dvo = new DataVO();
+        
+    }
+    
+    // 공지 디테일
+    NoticeVO getNoticeDetail(String notice_idx);
+    
+    // 공지 작성
+    int postNoticeJoin(NoticeVO nvo);
+    
+    // 공지 삭제(실제로는 update)
+    int putNoticeDelete(NoticeVO nvo);
 }
